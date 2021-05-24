f84f3fc649bc95307b7fa17214c663a762e14628
smilagros <1susanapuede2>
2021-05-24 09:13:41 -0500

    Taller 4 Repaso condicionales Funciones

---
+++ Domingo-Susana/Taller4/funciones.py
@@ -0,0 +1,46 @@
+# PCaculadora. Ejemplo de funciones
+
+# Suma
+def suma(x, y):
+    return x + y
+
+# Resta
+def resta(x, y):
+    return x - y
+
+# Multiplica
+def multiplicar(x, y):
+    return x * y
+
+n1 = float(input("Introduce tu primer número: "))
+n2 = float(input("Introduce tu segundo número: "))
+
+opcion = 0
+while True:
+    print("""
+    Dime, ¿qué quieres hacer?
+
+    1) Sumar los dos números
+    2) Restar los dos números
+    3) Multiplicar los dos números
+    4) Cambiar los números elegidos
+    5) Apagar calculadora
+    """)
+    opcion = int(input("Elige una opción: "))
+
+    if opcion == 1:
+        print(" ")
+        print("RESULTADO: La suma de", n1, "+", n2, "es igual a", suma(n1, n2))
+    elif opcion == 2:
+        print(" ")
+        print("RESULTADO: La resta de", n1, "-", n2, "es igual a", resta(n1, n2))
+    elif opcion == 3:
+        print(" ")
+        print("RESULTADO: El producto de", n1, "*", n2, "es igual a", multiplicar(n1, n2))
+    elif opcion == 4:
+        n1 = float(input("Introduce tu primer número: "))
+        n2 = float(input("Introduce tu segundo número: "))
+    elif opcion == 5:
+        break
+    else:
+        print("Opción incorrecta")
